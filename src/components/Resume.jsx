ec763e80f50c4875f4b37a696b9711906fdce958
rgremaud <rgremau@gmail.com>
2026-07-29 22:06:19 -0400

    Update formatting of resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,42 +5,47 @@ export default function Resume({ general, skills, education, experience }) {
   return (
     <div className="finalResume">
       <div className="header">
-          {general.firstName} {general.lastName}
+        {general.firstName} {general.lastName}
       </div>
       <div className="sidebar">
-          <div>
-            <img src={Mail} alt="Email" />
-            {general.email}
-          </div>
-          <div>
-            <img src={Phone} alt="Phone" />
-            {general.phone}
-          </div>
-          {skills.map((skill) => (
-            <div key={skill.id}>{skill.skill}</div>
-          ))}
-      </div>
-      <div className="educationFinal">
+        <div className="sidebarLine">
+          <img src={Mail} alt="Email" />
+          {general.email}
+        </div>
+        <div className="sidebarLine">
+          <img src={Phone} alt="Phone" />
+          {general.phone}
+        </div>
+        <div className="educationFinal">
+          <h3>Education</h3>
           {education.map((entry) => (
             <div key={entry.id}>
-              <h2>
-                {entry.school} - {entry.start} to {entry.end}
-              </h2>
+              <div> {entry.school} </div>
+              <div>
+                {" "}
+                {entry.start} to {entry.end}{" "}
+              </div>
               <p>{entry.major}</p>
             </div>
           ))}
+        </div>
+        <h3>Skills</h3>
+        {skills.map((skill) => (
+          <div key={skill.id}>{skill.skill}</div>
+        ))}
       </div>
       <div className="experienceFinal">
-          {experience.map((entry) => (
-            <div key={entry.id}>
-              <h2>
-                {entry.employer} - {entry.wstart} to {entry.wend}{" "}
-              </h2>
-              <p>• {entry.bulletOne}</p>
-              <p>• {entry.bulletTwo}</p>
-              <p>• {entry.bulletThree}</p>
-            </div>
-          ))}
+        <h1>Experience</h1>
+        {experience.map((entry) => (
+          <div key={entry.id}>
+            <h2>
+              {entry.employer} - {entry.wstart} to {entry.wend}{" "}
+            </h2>
+            <p>• {entry.bulletOne}</p>
+            <p>• {entry.bulletTwo}</p>
+            <p>• {entry.bulletThree}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
